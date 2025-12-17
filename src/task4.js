7fc973a0119343a9a404912bb3383fb1bdbab558
Duque-Paola <paosaldan@gmail.com>
2025-12-17 17:11:53 -0600

    Error en NPM para testear, codigo realizados y completados.

--- src/task4.js
+++ src/task4.js
@@ -1,10 +1,15 @@
 const arr = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14]
 
 // Escribe tu codigo por debajo de esta linea
-
-
+for (i = 1; i <= arr.length; i++) {
+  if(arr[i] == i){
+    arr.splice(i, 1)
+    console.log(arr);
+  }
+}
 
 // TEscribe tu codigo por encima de esta linea
+
 
 //No tocar esta funcion
 export function taskArr(){
--- src/task7.js
+++ src/task7.js
@@ -5,7 +5,13 @@ const arr = [
   ]
   
   // Type your code below this line!
-  
-  
-  
+  arr[1].splice(2,0,99);
+  console.log(arr);
+  /***************** */
+  const arrNew = [30,31,32,33,34,35,36,37,38,39]
+  arr.push(arrNew);
+  console.log(arr);
+  /**************** */
+  arr.splice(3, 4);
+  console.log(arr);
   // Type your code above this line!
